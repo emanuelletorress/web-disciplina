e44ce40db2d82e5103dd079e960a4228dbfe6752
manu <emanuelletorres982@gmail.com>
2023-05-07 08:53:23 -0300

    Exportando e importando multiplos componentes

---
+++ introducao/src/components/04MultiploComponente.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Isso é um cabeçalho</h1>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div>
+            <h1>Isso é um corpo</h1>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Isso é um rodapé</h1>
+        </div>
+    )
+}
+
+export {Header, Body, Footer}
